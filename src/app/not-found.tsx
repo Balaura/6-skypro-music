7f94986444f40b92f7bef543e29292061fadd9ef
Alexandra <  >
2024-08-06 05:38:47 +0300

    lesson files

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+     return (
+          <div>
+               <h1>404 - Not Found</h1>
+               <link rel="stylesheet" href="/">На главную</link>
+          </div>
+     );
+}
