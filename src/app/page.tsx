3c83ac742ff1168670c65bc990e5932d28f079e3
Alexandra <  >
2024-08-17 18:07:47 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Centerblock from '@/components/Centerblock/Centerblock';
 import Sidebar from '@/components/Sidebar/Sidebar';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.mainContent}>
       <Centerblock />
       <Sidebar />
-    </>
+    </div>
   );
 }
