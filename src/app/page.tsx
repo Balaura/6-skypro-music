897614d4271f034e7a794bf1de6377a94331c2fe
Alexandra <  >
2024-08-17 18:01:22 +0300

    reFix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import Navigation from '@/components/Navigation/Navigation';
 import Centerblock from '@/components/Centerblock/Centerblock';
 import Sidebar from '@/components/Sidebar/Sidebar';
-import Bar from '@/components/Bar/Bar';
-import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <Navigation />
-          <Centerblock />
-          <Sidebar />
-        </main>
-        <Bar />
-        <footer className={styles.footer}></footer>
-      </div>
-    </div>
+    <>
+      <Centerblock />
+      <Sidebar />
+    </>
   );
 }
